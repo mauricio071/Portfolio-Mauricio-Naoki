20987b3d757bb7f0194f0b5e71efbd66ff8014d1
Maurício Naoki <naokimau@gmail.com>
2024-09-17 11:16:38 -0300

    nodemailer adicionado e web3form removido

--- server/api/contact.post.js
+++ server/api/contact.post.js
@@ -1,21 +1,42 @@
+import nodemailer from 'nodemailer';
+
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
 
-    try {
-        const response = await $fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: {
-                access_key: process.env.WEB3FORMS_ACCESS_KEY,
-                subject: "Contato pelo portfolio",
-                name: body.name,
-                email: body.email,
-                message: body.message,
-            },
-        });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_KEY
+        },
+    });
 
-        return { status: 'sucesso', message: 'Email enviado com sucesso!', description: 'Sua mensagem foi enviada e em breve estarei respondendo. Obrigado pelo contato!' };
+    const mailOptions = {
+        from: body.email,
+        to: process.env.EMAIL_USER,
+        subject: `Contato de ${body.name} pelo portfolio`,
+        html: `
+            <h1>Novo contato recebido pelo portfolio</h1>
+            <p><strong>Nome:</strong> ${body.name}</p>
+            <p><strong>E-mail:</strong> ${body.email}</p>
+            <p><strong>Mensagem:</strong></p>
+            <p>${body.message}</p>
+        `,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+
+        return {
+            status: 'sucesso',
+            message: 'Email enviado com sucesso!',
+            description: 'Sua mensagem foi enviada e em breve estarei respondendo. Obrigado pelo contato!'
+        };
     } catch (error) {
-        return { status: 'erro', message: 'Erro ao enviar a mensagem!', description: 'Houve um problema ao tentar enviar a mensagem. Tente novamente mais tarde!' };
+        return {
+            status: 'erro',
+            message: 'Erro ao enviar a mensagem!',
+            description: 'Houve um problema ao tentar enviar a mensagem. Tente novamente mais tarde!'
+        };
     }
 });
