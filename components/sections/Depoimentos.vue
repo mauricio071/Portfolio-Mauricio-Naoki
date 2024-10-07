d2d0b8db752c07dd66c07558f1fed18923a01fb7
Maurício Naoki <naokimau@gmail.com>
2024-10-07 12:21:43 -0300

    svg das estrelas do depoimento atualizado

--- components/sections/Depoimentos.vue
+++ components/sections/Depoimentos.vue
@@ -25,7 +25,12 @@
                                         {{ depoimento.descricao }}
                                     </p>
                                     <div class="ratings">
-                                        <Icon v-for="i in 5" :key="i" name="mdi:star" class="text-[#ffc107]" />
+                                        <svg v-for="i in 5" :key="i" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24" fill="#ffc107">
+                                            <path fill-rule="evenodd"
+                                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                                clip-rule="evenodd" />
+                                        </svg>
                                     </div>
                                 </div>
                             </slide>
@@ -99,7 +104,7 @@ const breakpoints = {
     @apply flex gap-1 lg:pl-1;
 }
 
-.ratings span {
+.ratings svg {
     @apply w-[24px] h-[24px];
 }
 
