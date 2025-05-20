b03ac9e261b922efa5825d976089d1e24135c6ac
Maurício Naoki <naokimau@gmail.com>
2025-05-20 18:31:11 -0300

    refactor: espaçamento no menu do mobile melhorado

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -12,7 +12,7 @@
             <Icon @click="isOpen = !isOpen" name="material-symbols:menu-rounded"
                 class="text-[2rem] lg:hidden w-10 h-10 cursor-pointer" />
             <ul :class="{ 'hidden': !isOpen, 'flex': isOpen }"
-                class="flex-col items-center gap-5 w-full py-4 lg:gap-4 lg:flex lg:flex-row lg:justify-between lg:w-[unset] lg:my-[unset] xl:gap-5">
+                class="flex-col items-center gap-6 w-full pt-7 pb-4 lg:pt-4 lg:gap-4 lg:flex lg:flex-row lg:justify-between lg:w-[unset] lg:my-[unset] xl:gap-5">
                 <li v-for="(item, i) in items" :key="i">
                     <a @click="isOpen = false" :href="`#${item}`" v-motion-fade-visible :delay="i * 100"
                         :class="{ 'active': currentSection === item }" class="capitalize">
