5f85b91582b64a1083df23d255d89870837dd499
Maurício Naoki <naokimau@gmail.com>
2025-05-20 16:53:37 -0300

    refactor: melhoria na header em geral

--- components/sections/Inicio.vue
+++ components/sections/Inicio.vue
@@ -4,17 +4,17 @@
         </div>
         <div class="vector-background">
         </div>
-        <div class="wrapper relative my-16 lg:flex lg:justify-between lg:items-center">
+        <div class="wrapper relative my-16 lg:flex lg:justify-between lg:items-center max-sm:px-0">
             <div class="svg-wrapper">
-                <IconTecnologia class="header-vector" />
+                <IconTecnologia v-motion-fade-visible-once :duration="1300" :delay="850" class="header-vector" />
             </div>
-            <div v-motion-fade-visible-once :duration="1000"
-                class="main-content flex flex-col items-center gap-6 lg:items-start lg:gap-4">
+            <div v-motion-fade-visible-once :duration="1000" class="main-content">
                 <p class="text-white font-semibold text-lg lg:text-2xl">👋🏻 Olá, o meu nome é</p>
-                <h1 class="text-secondary font-bold text-3xl leading-4 sm:text-4xl lg:text-5xl  2xl:text-[3.5rem]">
+                <h1 class="text-secondary font-bold text-4xl lg:text-5xl 2xl:text-[3.5rem]">
                     Maurício Naoki
                 </h1>
-                <h2 class="flex items-center text-secondary text-xl font-bold sm:text-xl lg:text-2xl">
+                <h2
+                    class="flex items-center w-full text-secondary text-xl font-bold max-lg:max-w-[18rem] lg:w-fit lg:text-2xl">
                     <IconCode />
                     <span class="text-animation">
                         Desenvolvedor Front-end
@@ -41,7 +41,7 @@
                     </a>
                 </div>
             </div>
-            <div v-motion-fade-visible-once :duration="1300" :delay="200" ref="tiltRef" class="img-content">
+            <div v-motion-fade-visible-once :duration="1300" :delay="450" ref="tiltRef" class="img-content">
                 <div class="hidden max-w-[30rem] lg:block xl:max-w-[34rem] 2xl:max-w-[39rem]">
                     <NuxtImg src="/programacao.webp" alt="desenvolvedor" densities="x1" width="100%"
                         class="w-full duration-700" />
@@ -85,81 +85,6 @@ onMounted(() => {
         }
     }
 
-    .text-animation {
-        @apply w-full whitespace-nowrap overflow-hidden border-r-2 border-secondary pr-1;
-        animation: typing 5.57s steps(20) infinite,
-            blinking .85s infinite alternate;
-    }
-
-    @keyframes typing {
-        0% {
-            width: 0;
-        }
-
-        30%,
-        75% {
-            width: 100%;
-        }
-
-        100% {
-            width: 0;
-        }
-    }
-
-    @keyframes blinking {
-        50% {
-            @apply border-transparent;
-        }
-    }
-
-    .main-content::before {
-        @apply content-[""] absolute top-8 w-full h-[80%] bg-gradient-to-br from-[#00BFA6] to-[#63e6d2] opacity-20 z-[-1] blur-[75px];
-    }
-
-    .img-content {
-        @apply relative;
-
-        &::before {
-            @apply content-[""] absolute top-[15%] left-[15%] w-[70%] h-[70%] bg-gradient-to-br from-[#00BFA6] to-[#63e6d2] opacity-40 z-[-1] blur-[75px];
-        }
-    }
-
-    .resume-btn {
-        @apply relative overflow-hidden flex gap-2 items-center bg-white px-4 py-[.675rem] rounded-md text-secondary font-semibold my-2 shadow-sm duration-300 z-[2] md:my-5 md:py-3;
-
-        &::before {
-            @apply content-[""] absolute w-0 h-full top-0 left-0 bg-[#00bfa6] z-[-1] duration-500;
-        }
-
-        &:hover {
-            @apply text-white;
-            box-shadow: 0 0 30px #00bfa6,
-                0 0 20px #00bfa6;
-
-            span {
-                @apply text-white;
-            }
-
-            &::before {
-                @apply w-full;
-            }
-        }
-    }
-
-    .header-vector {
-        animation: circle 40s linear infinite;
-    }
-
-    @keyframes circle {
-        0% {
-            transform: rotate(0deg)
-        }
-
-        100% {
-            transform: rotate(-360deg)
-        }
-    }
-
     .svg-wrapper {
         @apply absolute max-w-[14rem] my-[3.5rem] top-[50%] right-[-15%] w-full h-full;
 
@@ -179,10 +104,91 @@ onMounted(() => {
             @apply max-w-[30rem] top-[-23%];
         }
 
-
         &::before {
             @apply content-[""] absolute top-[15%] left-[15%] w-[70%] h-[70%] bg-gradient-to-br from-[#00BFA6] to-[#63e6d2] z-[-1] opacity-40 blur-[20px] lg:blur-[75px];
         }
+
+        .header-vector {
+            animation: circle 40s linear infinite;
+        }
+
+        @keyframes circle {
+            0% {
+                transform: rotate(0deg)
+            }
+
+            100% {
+                transform: rotate(-360deg)
+            }
+        }
     }
+
+    .main-content {
+        @apply flex flex-col items-center gap-6 lg:items-start lg:gap-4;
+
+        &::before {
+            @apply content-[""] absolute top-8 w-full h-[80%] bg-gradient-to-br from-[#00BFA6] to-[#63e6d2] opacity-20 z-[-1] blur-[75px];
+        }
+
+        .text-animation {
+            @apply w-0 whitespace-nowrap overflow-hidden border-r-2 border-secondary;
+            animation: typing 5.5s steps(21) infinite,
+                blinking .85s infinite alternate;
+            animation-delay: 3s, 0s;
+
+            @keyframes typing {
+                0% {
+                    @apply w-0;
+                }
+
+                30%,
+                65% {
+                    @apply w-full pr-1;
+                }
+
+                87%,
+                100% {
+                    @apply w-0;
+                }
+            }
+
+            @keyframes blinking {
+                50% {
+                    @apply border-transparent;
+                }
+            }
+        }
+
+        .resume-btn {
+            @apply relative overflow-hidden flex gap-2 items-center bg-white px-4 py-[.675rem] rounded-md text-secondary font-semibold my-2 shadow-sm duration-300 z-[2] md:my-5 md:py-3;
+
+            &::before {
+                @apply content-[""] absolute w-0 h-full top-0 left-0 bg-[#00bfa6] z-[-1] duration-500;
+            }
+
+            &:hover {
+                @apply text-white;
+                box-shadow: 0 0 30px #00bfa6,
+                    0 0 20px #00bfa6;
+
+                span {
+                    @apply text-white;
+                }
+
+                &::before {
+                    @apply w-full;
+                }
+            }
+        }
+    }
+
+    .img-content {
+        @apply relative;
+
+        &::before {
+            @apply content-[""] absolute top-[15%] left-[15%] w-[70%] h-[70%] bg-gradient-to-br from-[#00BFA6] to-[#63e6d2] opacity-40 z-[-1] blur-[75px];
+        }
+    }
+
 }
 </style>
