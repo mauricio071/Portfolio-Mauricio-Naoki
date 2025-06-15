fccb302a517f002b7b4ea3bbad6ea7270dba3eb1
Maurício Naoki <naokimau@gmail.com>
2025-06-15 12:49:14 -0300

    layout e pagers removidos e melhoria na imagem da bandeira

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  // pages: false,
+  pages: false,
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/google-fonts",
